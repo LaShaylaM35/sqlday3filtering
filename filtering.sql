76c38f211526a47a29c9189edc404b8c6872394f
LaShaylaM35 <lashaylam@yahoo.com>
2026-03-23 02:37:56 -0500

    first commit

---
+++ filtering.sql
@@ -0,0 +1,13 @@
+SELECT * FROM products
+
+SELECT * FROM products
+where price between 1000 and 2000
+
+select count (*)
+from sales
+
+SELECT SUM(Quantity)
+FROM sales;
+
+SELECT AVG(quantity)
+FROM sales;
